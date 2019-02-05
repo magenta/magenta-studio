8cebeecd2eb6b7302eaf819b8884762d8f63aae0
tambien <yotammann@gmail.com>
2019-02-04 19:41:21 -0500

    using apps.json to track plugins

--- components/copy.js
+++ components/copy.js
@@ -1,8 +1,12 @@
 const fs = require('fs-extra')
 const { resolve } = require('path')
+const apps = require('../apps.json')
 
 async function main(){
-	const promises = ['continue', 'generate', 'groovae', 'interpolate'].map(f => {
+	// const folders = glob.sync(resolve(__dirname, '../*/Config.json'))
+	const dirs = Object.keys(apps)
+	
+	const promises = dirs.map(f => {
 		const components = resolve(__dirname, '../build/components.js')
 		const outFolder = resolve(__dirname, '../', f, 'build/components.js')
 		return fs.copy(components, outFolder)
